76f1f880fd1364a02fe7600dd2ae08564e35c5ed
lookingpig <lookingpig@gmail.com>
2014-07-09 16:23:13 +0800

    增加预约服务表

--- sql/DurkAuto_Shops.sql
+++ sql/DurkAuto_Shops.sql
@@ -31,11 +31,27 @@ CREATE TABLE `durkauto_appointment_type` (
 	`id` bigint(20) NOT NULL AUTO_INCREMENT,
 	`type_name` varchar(32) NOT NULL,
 	`parking` int(4) NOT NULL,
+	`business_date` int(3),
+	`business_hours_start` time,
+	`business_hours_end` time,
 	`service_time` int(4) NOT NULL,
 	`reminder_time` int(4) NOT NULL, 
 	`Wait_time` int(4) NOT NULL,
 	`time_basis` varchar(10) NOT NULL,
 	`exclusive` boolean NOT NULL,
+	`enable` boolean not null,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Create a appointment table
+DROP TABLE IF EXISTS `durkauto_appointment_service`;
+CREATE TABLE `durkauto_appointment_service` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`service_type` bigint(20) not null,
+	`appoint_date` date not null,
+	`appoint_time` time not null,
+	`arrival_time` time,
+	`reminder_time` time not null,
+	`service_state` int(1) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
